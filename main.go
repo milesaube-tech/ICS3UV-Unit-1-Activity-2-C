72a09f6688ce329854366b7b9c0ae305b105d8b6
default <miles.aube@stu.ocsb.ca>
2025-09-16 23:15:20 +0000

    added my new string literal programs

---
+++ main.go
@@ -0,0 +1,17 @@
+/*
+* Author: Miles Aube 
+* Version: 1.0.0
+* Date: 2025-09-16
+* This is my string literals code
+*/
+package main
+
+import "fmt"
+
+func main () {
+	//string literal text
+	fmt.Println("I love string literals in go")
+
+	//string literal text 
+	fmt.Println("Another string         literal for fun!")
+}
---
+++ main.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Miles Aube 
+ * @version 1.0.0
+ * @date 2025-09-16
+ * @fileoverview This is a program to practice string literals
+ */
+
+// string literal text 
+console.log("I love practicing string literals");
+
+// string literal text 
+console.log("    I love practecing string literals!!");
